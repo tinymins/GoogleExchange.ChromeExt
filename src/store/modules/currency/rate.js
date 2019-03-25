56c885b5aa66a71ce15182b4b0e0e38ae2ca9607
Emil Zhai <root@derzh.com>
2019-03-25 21:45:14 +0800

    fix: chart display

--- src/store/modules/currency/rate.js
+++ src/store/modules/currency/rate.js
@@ -8,6 +8,7 @@
 /* eslint no-param-reassign: ["error", { "props": false }] */
 
 import cheerio from 'cheerio';
+import escapeRegExp from 'lodash/escapeRegExp';
 import { setLocal, getLocal } from '@/utils/storage';
 import * as api from '@/store/api/currency';
 import { CURRENCY } from '@/store/types';
@@ -56,7 +57,24 @@ export default {
     [CURRENCY.RATE_SUCCESS](state, html) {
       const $ = cheerio.load(html);
       state.rate = $('#knowledge-currency__tgt-amount').data('value');
-      // state.chart = data.CurrencyUpdate[2];
+      const regexFromCurrency = new RegExp(`${escapeRegExp(state.fromCurrency)}\\s*\\((\\w+)\\)`, 'iu');
+      const regexToCurrency = new RegExp(`${escapeRegExp(state.toCurrency)}\\s*\\((\\w+)\\)`, 'iu');
+      let fromCurrencyCode = '';
+      let toCurrencyCode = '';
+      $('em').each((i, el) => {
+        const text = $(el).parent().text();
+        const resFromCurrency = regexFromCurrency.exec(text);
+        if (resFromCurrency) {
+          fromCurrencyCode = resFromCurrency[1];
+        }
+        const resToCurrency = regexToCurrency.exec(text);
+        if (resToCurrency) {
+          toCurrencyCode = resToCurrency[1];
+        }
+      });
+      state.chart = fromCurrencyCode && toCurrencyCode
+        ? `http://www.google.com/finance/chart?q=CURRENCY:${fromCurrencyCode}${toCurrencyCode}&tkr=1&p=5Y&chst=cob`
+        : 'http://www.google.com/finance/chart?q=CURRENCY';
       state.lock = false;
     },
     [CURRENCY.RATE_FAILURE](state) {
